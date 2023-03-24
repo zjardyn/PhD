199bbc785c7cca7620fde5b332eccf050aca78c1
zjardyn <zjardyn@gmail.com>
2023-03-24 13:39:49 -0400

    added stuff for presentation

--- 16S/full_enrichment_analysis.R
+++ 16S/full_enrichment_analysis.R
@@ -1,13 +1,13 @@
 library(ggthemes)
 library(RColorBrewer)
 library(glue)
+library(stringr.tools)
 # load my functions
-# source("C:/Users/Zjardyn/Desktop/PhD/16S/16S_functions.R")
-source("C:/Users/zjard/Desktop/PhD/16S/16S_functions.R")
+source("C:/Users/Zjardyn/Desktop/PhD/16S/16S_functions.R")
 
 # ---- Loading Data ----
-all_tbl <- "C:/Users/zjard/Desktop/full_out/full-merged-table.qza"
-all_tx <- "C:/Users/zjard/Desktop/full_out/full-merged-taxonomy.qza"
+all_tbl <- "C:/Users/Zjardyn/Desktop/full_out/full-merged-table.qza"
+all_tx <- "C:/Users/Zjardyn/Desktop/full_out/full-merged-taxonomy.qza"
 all_sums <- taxa_sums(table = all_tbl, taxonomy = all_tx)
 all_tab <- taxa_prop_table(taxasums = all_sums, taxa = "Genus")
 
@@ -100,7 +100,7 @@ all_tab <- all_tab %>%
   mutate(smp = str_replace_all(smp, "^July7\\.Styrene\\.3", glue("Sty_3_{lubridate::ymd(20220707)}"))) %>%
   mutate(smp = str_replace_all(smp, "^July7\\.Styrene\\.4", glue("Sty_4_{lubridate::ymd(20220707)}"))) %>%
   # Sty June21
-  mutate(smp = str_replace_all(smp, "^June21 \\.Styrene\\.1", glue("Sty_1_{lubridate::ymd(20220621)}"))) %>%
+  mutate(smp = str_replace_all(smp, "^June21\\.Styrene\\.1", glue("Sty_1_{lubridate::ymd(20220621)}"))) %>%
   mutate(smp = str_replace_all(smp, "^June21\\.Styrene\\.2", glue("Sty_2_{lubridate::ymd(20220621)}"))) %>%
   mutate(smp = str_replace_all(smp, "^June21\\.Styrene\\.3", glue("Sty_3_{lubridate::ymd(20220621)}"))) %>%
   mutate(smp = str_replace_all(smp, "^June21\\.Styrene\\.4", glue("Sty_4_{lubridate::ymd(20220621)}"))) %>%
@@ -122,56 +122,47 @@ all_tab <- all_tab %>%
   column_to_rownames(var = "smp")
 
 # ---- Soils ----
+soil_thresh <- 3
 soils_filt <- rownames(all_tab)[str_detect(rownames(all_tab), "Soil")]
 soils_tab <- filter_samples_table(soils_filt, all_tab)
-soils_tab_thresh <- thresh_prop_table(soils_tab, threshold = 5)
+soils_tab_thresh <- thresh_prop_table(soils_tab, threshold = soil_thresh)
 soils_tbm <- as_tibble(melt(as.matrix(soils_tab_thresh)))
 
+soils_tbm <- soils_tbm %>%
+  filter(Var1 == "Soil.1_1_2022-06-07" | 
+         Var1 == "Soil.2_3_2022-06-07" | 
+         Var1 == "Soil.3_3_2022-06-20" | 
+         Var1 == "Soil.4_2_2022-06-20" )
+  
 soils_tbm <- arrange_taxa(soils_tab_thresh, soils_tbm)
 
 soils_tbm_v <- soils_tbm %>% 
     separate_wider_delim(Var1, delim = "_", names = c("Sample","Replicate", "Date"), cols_remove = FALSE)
 
-library(viridis)
-all_cols_blind <- viridis::viridis.map %>%
-  filter(opt == "A")
-all_cols_blind <- all_cols_blind[30:216,]
-# all_cols_blind1 <- all_cols_blind %>%
-#   filter(opt == "A")
-# all_cols_blind1 <- all_cols_blind1[40:236,]
-# 
-# all_cols_blind2 <- all_cols_blind %>%
-#   filter(opt == "G")
-# all_cols_blind2 <- all_cols_blind2[40:236,]
-# 
-# all_cols_blind <- rbind(all_cols_blind1, all_cols_blind2)
-all_cols_blind_hex <- rgb(all_cols_blind[,1],all_cols_blind[,2],all_cols_blind[,3])
+# soils_cols <- viridis_colours(soils_tbm_v, seed = 1, start_trunc = 30, end_trunc = 0)
 
-all_cols_blind_hex <- all_cols_blind_hex[c(TRUE, FALSE)]
-
-num_col <- soils_tbm %>%
-  summarise(orgs = unique(Var2)) %>%
-  nrow()
-
-set.seed(5)
-new_cols <- sample(all_cols_blind_hex, num_col)
-new_cols[1] <- "#808080"
+soils_cols <-lrg_colors(length(unique(soils_tbm_v$Var2)), seed = 43)
 
 soils_tbm_v %>%
-  ggplot(aes(fill=Var2, y=value, x=Var1)) +
+  mutate(Sample = str_replace_all(Sample, "\\.", " ")) %>%
+  ggplot(aes(fill=Var2, y=value, x=Sample)) +
   geom_bar(position="fill", stat="identity") +
   theme(axis.text.x= element_text(angle = 90, hjust = 1)) +
-  scale_fill_manual(values = new_cols)
- # # remove controls
-# smp_filt = rownames(all_tab)[!rownames(all_tab) == 'C']
-# all_tab_f <- filter_samples_table(smp_filt = smp_filt, table = all_tab)
-# View(all_tab_f)
-
+  scale_fill_manual(values = soils_cols) +
+  guides(fill=guide_legend(title="Genus")) +
+  labs(y = "Abundance", title = glue("16S community composition of soil samples. Threshold: {soil_thresh}%")) +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 11),
+        axis.title = element_text(size = 13),
+        legend.title = element_text(size = 13),
+        legend.text = element_text(size = 10))
 
 # ---- Enrichments ----
+enrich_thresh = 6
 enrich_filt <- rownames(all_tab)[str_detect(rownames(all_tab), "^Sty_|^Hex_" )]
 enrich_tab <- filter_samples_table(enrich_filt, all_tab)
-enrich_tab_thresh <- thresh_prop_table(enrich_tab, threshold = 10)
+enrich_tab_thresh <- thresh_prop_table(enrich_tab, threshold = enrich_thresh)
 enrich_tbm <- as_tibble(melt(as.matrix(enrich_tab_thresh)))
 enrich_tbm <- arrange_taxa(enrich_tab_thresh, enrich_tbm)
 
@@ -180,7 +171,6 @@ enrich_tbm_v <- enrich_tbm %>%
   separate_wider_delim(Var1, delim = "_", names = c("Treatment", "Soil", "Date"))
 
 ### Hexadecane ###
-
 enrich_hex <- enrich_tbm_v %>%
     filter(Treatment == "Hex") %>%
     filter(value != 0.0)
@@ -194,23 +184,29 @@ enrich_tab_hex <- enrich_tab_thresh %>%
 
 enrich_hex <- arrange_taxa(enrich_tab_hex, enrich_hex)
 
-num_col_hex <- enrich_hex %>%
-    summarise(orgs = unique(Var2)) %>%
-    nrow()
+enrich_hex <- enrich_hex %>%
+  mutate(Soil = str_prefix(Soil, "Soil "))
 
-set.seed(4)
+hex_colours <-lrg_colors(length(unique(enrich_hex$Var2)), seed = 30)
 
-new_cols_hex <- sample(all_cols_blind_hex, num_col2)
-new_cols_hex[1] <- "#808080"
-    
 enrich_hex %>%
-    ggplot(aes(fill=Var2, y=value, x=Date)) + 
-    geom_bar(position="fill", stat="identity") + 
-    theme(axis.text.x= element_text(angle = 90, hjust = 1)) + 
-    scale_fill_manual(values = new_cols_hex) +
-    facet_grid(Treatment~Soil) 
-### Styrene ###
+  ggplot(aes(fill=Var2, y=value, x=Date)) + 
+  geom_bar(position="fill", stat="identity") + 
+  theme(axis.text.x= element_text(angle = 90, hjust = 1)) + 
+  scale_fill_manual(values = hex_colours) +
+  facet_wrap(~Soil, nrow = 1) +
+  guides(fill=guide_legend(title="Genus")) +
+  labs(y = "Abundance", title = glue("16S community composition of hexadecane enrichments. Threshold: {enrich_thresh}%")) + 
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 10,angle = 35, vjust = 1, hjust=1),
+        axis.text.y = element_text(size = 10),
+        axis.title = element_text(size = 13),
+        legend.title = element_text(size = 13),
+        legend.text = element_text(size = 10),
+        strip.text.x = element_text(size = 11))
 
+### Styrene ###
 enrich_sty <- enrich_tbm_v %>%
     filter(Treatment == "Sty") %>%
     filter(value != 0.0) 
@@ -224,21 +220,27 @@ enrich_tab_sty <- enrich_tab_thresh %>%
 
 enrich_sty <- arrange_taxa(enrich_tab_sty, enrich_sty)
 
-num_col_sty <- enrich_sty %>%
-    summarise(orgs = unique(Var2)) %>%
-    nrow()
+enrich_sty <- enrich_sty %>%
+  mutate(Soil = str_prefix(Soil, "Soil "))
 
-set.seed(3)
-
-new_cols_sty <- sample(all_cols_blind_hex, num_col2)
-new_cols_sty[1] <- "#808080"
+sty_colours <-lrg_colors(length(unique(enrich_sty$Var2)), seed = 43)
 
 enrich_sty %>%
   ggplot(aes(fill=Var2, y=value, x=Date)) + 
   geom_bar(position="fill", stat="identity") + 
   theme(axis.text.x= element_text(angle = 90, hjust = 1)) + 
-  scale_fill_manual(values = new_cols_sty) +
-  facet_grid(Treatment~Soil) 
+  scale_fill_manual(values = sty_colours) +
+  facet_wrap(~Soil, nrow = 1) +
+  guides(fill=guide_legend(title="Genus")) +
+  labs(y = "Abundance", title = glue("16S community composition of styrene enrichments. Threshold: {enrich_thresh}%")) +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 10,angle = 35, vjust = 1, hjust=1),
+        axis.text.y = element_text(size = 10),
+        axis.title = element_text(size = 13),
+        legend.title = element_text(size = 13),
+        legend.text = element_text(size = 10),
+        strip.text.x = element_text(size = 11))
 
 # ---- DTE ---- 
 dte_filt <- rownames(all_tab)[str_detect(rownames(all_tab), "DTE")]
@@ -247,9 +249,11 @@ dte_tab_thresh <- thresh_prop_table(dte_tab, threshold = 0.0001)
 dte_tbm <- as_tibble(melt(as.matrix(dte_tab_thresh)))
 dte_tbm <- arrange_taxa(dte_tab_thresh, dte_tbm)
 
+dte_colours <- viridis_colours(dte_tbm, seed = 1, start_trunc = 30, end_trunc = 20)
+
 dte_tbm %>%
   ggplot(aes(fill=Var2, y=value, x=Var1)) +
   geom_bar(position="fill", stat="identity") +
   theme(axis.text.x= element_text(angle = 90, hjust = 1)) +
-  scale_fill_manual(values = new_cols)
+  scale_fill_manual(values = dte_colours)
 
